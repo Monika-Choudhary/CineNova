9e9d92617d42f697f1a35bd7ded7eb5f045a0037
Monika-Choudhary <monikachahota@gmail.com>
2025-02-18 09:34:53 +0100

    Refactor HTML structure and improve code formatting; enhance API fetch functions and event listener in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,30 @@
-import './style.css'
+import "./style.css";
 import { fetchMovies } from "./api";
 
 const searchInput = document.getElementById("search") as HTMLInputElement;
 
-  interface Movie {
-    id: number;
-    poster_path: string;
-    title: string;
-  }
+interface Movie {
+  id: number;
+  poster_path: string;
+  title: string;
+}
 
-  const moviesContainer = document.getElementById("movies") as HTMLDivElement;
+const moviesContainer = document.getElementById("movies") as HTMLDivElement;
 
-  searchInput.addEventListener("input", async () => {
-    const query = searchInput.value.trim();
-    if (!query) return;
+searchInput.addEventListener("input", async () => {
+  const query = searchInput.value.trim();
+  if (!query) return;
 
-    const movies: Movie[] = await fetchMovies(query);
+  const movies: Movie[] = await fetchMovies(query);
 
-    moviesContainer.innerHTML = movies
-      .map((movie: Movie) => `
+  moviesContainer.innerHTML = movies
+    .map(
+      (movie: Movie) => `
         <a href="movie.html?id=${movie.id}" class="block">
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="rounded">
           <h2 class="text-center mt-2">${movie.title}</h2>
         </a>
-      `)
-      .join("");
-  });
-
+      `
+    )
+    .join("");
+});
