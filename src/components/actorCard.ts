dc727c49cc5e1f86cf7aae89f714d8a15428f293
Monika-Choudhary <monikachahota@gmail.com>
2025-02-22 01:06:37 +0100

    Fix image alignment in actor card; update class for improved styling

--- src/components/actorCard.ts
+++ src/components/actorCard.ts
@@ -8,7 +8,7 @@ interface Actor {
 export function ActorCard(actor: Actor) {
   return `
        <div class="text-center">
-      <img src="https://image.tmdb.org/t/p/w185${actor.profile_path}" class=" rounded-l mx-auto">
+      <img src="https://image.tmdb.org/t/p/w185${actor.profile_path}" class=" rounded mx-auto">
       <p class="text-sm font-kantumruy  mt-2">${actor.name}</p>
       <p class="text-xs text-gray-400">as ${actor.character}</p>
     </div>
