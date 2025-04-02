77063f4756e9ebfc893dca041fca557e901982ec
Monika-Choudhary <monikachahota@gmail.com>
2025-04-02 13:10:12 +0200

    Remove hardcoded base URL from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: "/CineNova/", // Name deines GitHub-Repos
+// Name deines GitHub-Repos
   plugins: [
     tailwindcss(),
   ],
